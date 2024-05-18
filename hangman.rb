cfcc1f2586fb9b7c6c03206ca9619207406af6a2
egarci000 <egarci9000@gmail.com>
2024-05-18 16:15:48 -0500

    Add way of saving hangman games to a file, and then allowing a player to load a saved game file

--- hangman.rb
+++ hangman.rb
@@ -1,38 +1,16 @@
-#make everything neat
-#serialize :(
-
-#allow player to save game by pressing "!" at the start of any turn
-#search for way to serialize game class
-#when program loads, add option to allow you to open one of your saved games, which should arrive you at the correct positions you were at when you saved
-
-#to do:
-#serialize class using json - save file to a "saved-games" directory under Hangman directory.
-#              Allow user to give the file a name or choose a default one
-#start of every turn, direct user they can save game by pressing, "!", this will exit them out of the game, (cont.)
-#             save the Hangman class and its instance variables, then save them to a JSON file.
-#at the start of every game, allow the user to load in a saved game and display things correctly
-#add special scoring where a user receives points for how many empty spaces are left in the secret word when they enter the full secret word
-#       ex. word is cunning, user enters "cunning" when the word_display is: c-nn-ng, thus they get two points instead of 1
-
-#how to implement loading instance variables from file:
-#instance variables to be saved: @secret_word, @word_display, @guesses_left, @incorrect_letters,
-                                #@letters_entered, @wins_losses, @points
-#when user loads in file, unpack it, then call Hangman.new(input paramaters from file, in the order above)
-#give initialize optional parameters: def initialize(word=nil, word_display=nil, guesses_left=7, letters_entered=[],
-                                        #wins_losses=[0,0], points=0)
-#consult all knowing code god for further inquiries
-
+require 'json'
 
 class Hangman
-  def initialize
+  def initialize(word=nil, word_display=nil, guesses_left=nil, incorrect_letters=nil, letters_entered=nil, wins_losses=nil, points=nil, load_game=nil)
     @guess = ""
-    @secret_word = get_secret_word #|| word - place before method
-    @word_display = get_word_display #|| word_display - place before method
-    @guesses_left = 7
-    @incorrect_letters = []
-    @letters_entered = []
-    @wins_losses = [0, 0]
-    @points = 0
+    @secret_word =  word || get_secret_word
+    @word_display = word_display || get_word_display
+    @guesses_left = guesses_left || 7
+    @incorrect_letters = incorrect_letters || []
+    @letters_entered = letters_entered || []
+    @wins_losses = wins_losses || [0, 0]
+    @points = points || 0
+    @load_game = load_game || false
     @game_over = false
     @hangman_stages = [
   "    |-----
@@ -85,7 +63,9 @@ class Hangman
     |_________"
   ]
 
-    main_menu
+    main_menu if @load_game == false
+    get_guess if @load_game == true
+
   end
 
   def reset_variables
@@ -94,11 +74,11 @@ class Hangman
     @guesses_left = 7
     @incorrect_letters = []
     @letters_entered = []
+    @game_over = false
 
     get_guess
-  end
 
-  attr_accessor :secret_word
+  end
 
   def main_menu
     system("clear") || system("cls")
@@ -109,7 +89,7 @@ class Hangman
     when "1"
       get_guess
     when "2"
-      #load_game
+      load_game
     when "3"
       scoring_system
     when "4"
@@ -119,18 +99,94 @@ class Hangman
     end
   end
 
+  def save_game
+    obj = {}
+    instance_variables.map do |var|
+      unsaved_inst_vars = [:@guess, :@hangman_stages, :@game_over]
+      obj[var] = instance_variable_get(var) unless unsaved_inst_vars.include?(var)
+    end
+
+    print "Enter the file name you'd like your game to be saved under: "
+    name = gets.chomp
+
+    f = File.open("saved_games/#{name}.txt", "w")
+    f.puts JSON.dump obj
+
+    puts "Saved under #{name}.txt"
+  end
+
+  def load_game
+
+    if Dir.children("saved_games").empty? == false
+      system("clear") || system("cls")
+      puts "Enter m to go back to main menu\n\n"
+
+      Dir.children("saved_games").each do |file|
+        puts "#{Dir.children("saved_games").index(file)+1} #{file}"
+      end
+
+      print "\nEnter the number corresponding to the file you'd like to open: "
+      answer = gets.chomp
+
+      File.open("saved_games/#{Dir.children("saved_games").at(answer.to_i-1)}").each do |line|
+        $obj = JSON.parse(line)
+      end
+
+      begin
+        $obj.keys.each do |key|
+          instance_variable_set(key, $obj[key])
+        end
+      rescue NoMethodError => e
+        system("clear") || system("cls")
+        print "No saved game data found, enter b to go back to saved files: "
+        answer = gets.chomp.downcase
+
+        load_game if answer == "b"
+      else
+      end
+
+    else
+      system("clear") || system("cls")
+      puts "No saved games found\n\n"
+      print "Enter m to go back to the main menu: "
+      answer = gets.chomp
+
+      main_menu if answer == "m"
+    end
+
+
+
+    self.class.make_new_game($obj) if @game_over == false
+  end
+
+  def self.make_new_game(i_vars)
+    puts "was called"
+    arr_of_instance_values = i_vars.values
+    p arr_of_instance_values
+    puts @game_ove
+
+    Hangman.new(arr_of_instance_values.at(0), arr_of_instance_values.at(1),
+                arr_of_instance_values.at(2), arr_of_instance_values.at(3),
+                arr_of_instance_values.at(4), arr_of_instance_values.at(5),
+                arr_of_instance_values.at(6), true)
+  end
+
   def scoring_system
-    puts "In construction"
-    #explain scoring system
-    #puts "You receive points directly based on how many guesses were leftover. Points "
+    system("clear") || system("cls")
+    puts "You receive points based on how many guesses are leftover when the secret word is guessed.\n\n"
+    puts "If correctly entering the entire secret word as your guess you receive one point for every missing letter in the display of the secret word.\n\n"
+
+    print "Enter m to go back to the main menu:   "
+    answer = gets.chomp.downcase
+
+    main_menu if answer == "m"
   end
 
   def high_scores
-    #reads file "scores.txt and prints it to the screen"
     system("clear") || system("cls")
     File.foreach("scores.txt") {|line| puts line}
     puts "\nPress m to go back to the main menu"
-    answer = gets.chomp
+    answer = gets.chomp.downcase
     main_menu if answer == "m"
   end
 
@@ -151,8 +207,9 @@ class Hangman
     puts "\n\n"
     puts @word_display
     puts "\nincorrect guesses: #{@incorrect_letters.join(",")}\n\n"
-    if @guesses_left != 0
-      puts "Please enter your guess, guesses left: #{@guesses_left}. Press ! to save game "
+    if @game_over == false
+      puts "Enter ! to save game"
+      print "Please enter your guess:  "
       @guess = gets.chomp.downcase
 
       if @guess == "!"
@@ -169,12 +226,11 @@ class Hangman
   end
 
   def check_if_in_secret_word?(guess)
-    check_game if @guess == @secret_word && @game_over
     @secret_word.include?(guess) ? count_occurences(guess) : update_incorrect_letters(guess)
   end
 
   def count_occurences(guess)
-    index = (0 ... @secret_word.length).find_all { |i| @secret_word[i,1] == guess }
+    index = (0 ... @secret_word.length).find_all { |i| @secret_word[i, 1] == guess }
     update_word_display(index)
   end
 
@@ -198,7 +254,7 @@ class Hangman
   end
 
   def update_hangman
-    @hangman_stages[@guesses_left]
+    print "#{@hangman_stages[@guesses_left]}                guesses left:  #{@guesses_left}"
   end
 
   def check_game
@@ -207,17 +263,22 @@ class Hangman
       hash_of_tally = @word_display.split("").tally {|t| t == "-"}
       count = hash_of_tally["-"] + @guesses_left
       @points += count
+      @game_over = true
 
-      puts "\ncongrats you won!"
+      get_guess
+      puts "\nCongrats you won!"
       display_end_message
     elsif @word_display == @secret_word
       @wins_losses[0] += 1
       @points += @guesses_left
+      @game_over = true
 
-      puts "\ncongrats you won!"
+      get_guess
+      puts "\nCongrats you won!"
       display_end_message
     elsif @guesses_left == 0
       @wins_losses[1] += 1
+      @game_over = true
 
       get_guess
       puts "\nYou lost :("
@@ -228,31 +289,32 @@ class Hangman
   end
 
   def display_end_message
-    puts "\n \nSecret word was #{@secret_word}"
-    puts "guesses left: #{@guesses_left}"
+    puts "\n\nSecret word was #{@secret_word}"
+    puts "Guesses left: #{@guesses_left}"
     puts "Wins: #{@wins_losses[0]}, losses: #{@wins_losses[1]}. Points: #{@points}"
-    puts "Would you like to play again? y for yes, n for no"
+    print "\nWould you like to play again? y for yes, n for no:  "
     answer = gets.chomp.downcase
 
     if answer == "y"
       reset_variables
     else
-      @game_over = true
       save_score
     end
   end
 
   def save_score
-    puts "Would you like to save your score, along with your win/loss record? y for yes, n for no"
-    answer = gets.chomp
-    f = File.open("scores.txt", "a")
+    print "\nWould you like to save your score, along with your win/loss record? y for yes, n for no:  "
+    answer = gets.chomp.downcase
+
     if answer == "y"
+      f = File.open("scores.txt", "a")
       puts "Please enter your name: "
       name = gets.chomp
       f.puts "#{name} score: #{@points}, win/loss record: #{@wins_losses[0]}W, #{@wins_losses[1]}L"
-      puts "Score saved!"
+      puts "Score saved #{name}!"
     end
   end
+
 end
 
 new_game = Hangman.new
